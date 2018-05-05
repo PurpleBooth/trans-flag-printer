217114b02f06b743220d239c9c091c330a75447c
Billie Thompson <billie@billiecodes.com>
2018-05-05 08:50:22 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate termion;
+
+use termion::terminal_size;
+use termion::color;
+
+fn main() {
+    let (width, _) = terminal_size().unwrap();
+    let line_bar = "█".repeat(width as usize - 2);
+
+    let flag_blue = color::Fg(color::LightBlue);
+    let flag_pink = "\x1B[38;5;219m";
+    let flag_white = color::Fg(color::White);
+    let reset_style = color::Fg(color::Reset);
+
+    println!("{}{}{}", flag_blue, line_bar, reset_style);
+    println!("{}{}{}", flag_pink, line_bar, reset_style);
+    println!("{}{}{}", flag_white, line_bar, reset_style);
+    println!("{}{}{}", flag_pink, line_bar, reset_style);
+    println!("{}{}{}", flag_blue, line_bar, reset_style);
+}
